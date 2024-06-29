7dfd9dba4882e9d9a3014da4f8b1a4beb1f1db1a
Felipe Pedroso Ramos <felipe.pedroso-ramos@odt.net>
2024-06-29 11:04:09 +0200

    feat(66937423): add recommendations model, add data context, create initial migration,

---
+++ Project/Project/Models/Recommendation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Project.Models
+{
+	public class Recommendation
+	{
+        public int Id { get; set; }
+        public int User_id { get; set; }
+        public int Movie_id { get; set; }
+    }
+}
+
