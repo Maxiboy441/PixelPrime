63bb248264e2b58ce1066edb4824d8460d210ce7
Maximilian Huber <maximilian.huber@odt.net>
2024-06-30 22:52:14 +0200

    added migrations

--- Project/Project/Migrations/20240630205010_create_users_table_migration.Designer.cs
+++ Project/Project/Migrations/20240630205010_create_users_table_migration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.Data;
 
@@ -10,9 +11,11 @@ using Project.Data;
 namespace Project.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240630205010_create_users_table_migration")]
+    partial class create_users_table_migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
