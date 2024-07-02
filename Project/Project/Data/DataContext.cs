4239e03671d04ea55b7c038533ebaef12c3017d1
Maximilian Huber <maximilian.huber@odt.net>
2024-07-02 13:57:15 +0200

    feat(66937493): add automated dates

--- Project/Project/Data/DataContext.cs
+++ Project/Project/Data/DataContext.cs
@@ -20,6 +20,28 @@ namespace Project.Data
                 .Property(p => p.Id)
                 .ValueGeneratedOnAdd();
         }
+        
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries();
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is Rating)
+                {
+                    var now = DateTime.UtcNow;
+                    if (entry.State == EntityState.Added)
+                    {
+                        entry.Property("CreatedAt").CurrentValue = now;
+                        entry.Property("UpdatedAt").CurrentValue = now;
+                    }
+                    if (entry.State == EntityState.Modified)
+                    {
+                        entry.Property("UpdatedAt").CurrentValue = now;
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
 
