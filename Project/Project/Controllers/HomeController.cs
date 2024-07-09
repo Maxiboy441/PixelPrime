8bc021ec1167e75c32ddff41116c065e79d62917
Maximilian Huber <maximilian.huber@odt.net>
2024-07-09 12:48:23 +0200

    feat(66937177): Add Homepage and Controller

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -1,20 +1,34 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Project.Models;
+using Project.Data;
 
 namespace Project.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly DataContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
+    }
+    
+    public List<Rating> TopRated()
+    {
+        var TopRated = _context.Ratings
+            .OrderByDescending(r => r.Rating_value)
+            .Take(5)
+            .ToList();
+
+        return TopRated;
     }
 
     public IActionResult Index()
     {
+        ViewData["TopRated"] = TopRated();
         return View();
     }
 
---
+++ Project/Project/Services/ConvertModelService.cs
@@ -0,0 +1,71 @@
+using System.Globalization;
+using Project.Models;
+
+// Usage:
+// var convertService = new ConvertModelService();
+// List<Rating> ratings = (List<Rating>)ViewData["TopRated"];
+// List<Movie> moviesFromReviews = convertService.ConvertToMovies(ratings);
+
+namespace Project.Services;
+
+public class ConvertModelService
+{
+    public List<Movie> ConvertToMovies<T>(List<T> items) where T : class
+    {
+        return items.Select(ConvertToMovie).ToList();
+    }
+
+    private static Movie ConvertToMovie<T>(T item) where T : class
+    {
+        if (item is Review review)
+        {
+            return new Movie
+            {
+                Id = review.Movie_id,
+                Title = review.Movie_title,
+                Poster = review.Movie_poster
+            };
+        }
+        else if (item is Rating rating)
+        {
+            return new Movie
+            {
+                Id = rating.Movie_id,
+                Title = rating.Movie_title,
+                Poster = rating.Movie_poster,
+                PixelRating = rating.Rating_value.ToString(CultureInfo.CurrentCulture)
+            };
+        }
+        else if (item is Recommendation recommendation)
+        {
+            return new Movie
+            {
+                Id = recommendation.Movie_id,
+                Title = recommendation.Movie_title,
+                Poster = recommendation.Movie_poster
+            };
+        }
+        else if (item is Watchlist watchlist)
+        {
+            return new Movie
+            {
+                Id = watchlist.Movie_id,
+                Title = watchlist.Movie_title,
+                Poster = watchlist.Movie_poster
+            };
+        }
+        else if (item is Favorite favorites)
+        {
+            return new Movie
+            {
+                Id = favorites.Movie_id,
+                Title = favorites.Movie_title,
+                Poster = favorites.Movie_poster
+            };
+        }
+        else
+        {
+            throw new ArgumentException($"Unsupported type: {typeof(T).Name}");
+        }
+    }
+}
