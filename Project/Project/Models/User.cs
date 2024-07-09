36047acfbbd88c0d88e9697bdabe9e1fdfa35ac1
Felipe Pedroso Ramos <felipe.pedroso-ramos@odt.net>
2024-07-09 12:19:56 +0200

    feat(70072269): create auth views

---
+++ Project/Project/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [StringLength(255, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 50 characters")]
+        public string? Password { get; set; }
+
+        public string? Description { get; set; }
+        public string? Avatar { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+    }
+}
