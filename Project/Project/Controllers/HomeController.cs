a4d851ba99de1cfbce5c98d88b12831fa5569021
Maximilian Huber <maximilian.huber@odt.net>
2024-07-10 09:30:08 +0200

    fix(66937177): no duplicate Ratings

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -18,12 +18,39 @@ public class HomeController : Controller
     
     public List<Rating> TopRated()
     {
-        var TopRated = _context.Ratings
+        var movieIds = _context.Ratings.Select(r => r.Movie_id).Distinct().ToList();
+
+        var aggregatedRatings = new List<Rating>();
+
+        foreach (var movieId in movieIds)
+        {
+            var movieRatings = _context.Ratings.Where(r => r.Movie_id == movieId).ToList();
+
+            if (movieRatings.Count == 1)
+            {
+                aggregatedRatings.Add(movieRatings[0]);
+            }
+            else if (movieRatings.Count > 1)
+            {
+                var averageRating = new Rating
+                {
+                    Movie_id = movieId,
+                    Rating_value = Math.Round(movieRatings.Average(r => r.Rating_value),1),
+                    Movie_title = movieRatings[0].Movie_title,
+                    Movie_poster = movieRatings[0].Movie_poster,
+                    Created_at = DateTime.Now,
+                    Updated_at = DateTime.Now
+                };
+                aggregatedRatings.Add(averageRating);
+            }
+        }
+
+        var topRated = aggregatedRatings
             .OrderByDescending(r => r.Rating_value)
-            .Take(5)
+            .Take(10)
             .ToList();
 
-        return TopRated;
+        return topRated;
     }
 
     public IActionResult Index()
