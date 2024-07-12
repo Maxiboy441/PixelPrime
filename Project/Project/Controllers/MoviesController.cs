2f9d5282680bd72cdddf01aa57fd1d2e28957808
mariuswenzelits <marius.wenzel@its-stuttgart.de>
2024-07-12 14:34:22 +0200

    feat(66939895): initial Movie Controller Commit

---
+++ Project/Project/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public MoviesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StoreWatchlist(string movieId, string title, string poster)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var favorite = new Favorite
+            {
+                MovieId = movieId,
+                Title = title,
+                Poster = poster,
+                UserId = user.Id
+            };
+
+            _context.Favorites.Add(favorite);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Favorite added successfully." });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DestroyWatchlist(int favoriteId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var favorite = _context.Favorites.FirstOrDefault(f => f.FavoriteId == favoriteId && f.UserId == user.Id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            _context.Favorites.Remove(favorite);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Favorite removed successfully." });
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
