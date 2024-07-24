b52a6361a02ba2e62cfafab79fcac45c0e2410e0
Maximilian Huber <maximilian.huber@odt.net>
2024-07-24 08:42:46 +0200

    remove project (to solve conflicts)

--- Project/Project/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project.Models.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
