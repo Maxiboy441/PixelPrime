61b18a60388bc4a026a9370d1344199cd6f486a0
Maximilian Huber <maximilian.huber@odt.net>
2024-07-31 10:17:39 +0200

    refactor(72589015): change info to impressum naming

--- Project/Project/Controllers/HomeController.cs
+++ Project/Project/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Info()
+    public IActionResult Impressum()
     {
         return View();
     }
