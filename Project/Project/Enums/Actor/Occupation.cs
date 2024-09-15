ebb8bba6dd60b5552f9b86e0fca89af66dd6f3d7
Felipe Pedroso Ramos <felipe.pedroso-ramos@odt.net>
2024-09-15 10:29:31 +0200

    fix(79680908): create occupation enum, return null if celebrity does not have any occupation in the enum

---
+++ Project/Project/Enums/Actor/Occupation.cs
@@ -0,0 +1,41 @@
+namespace Project.Enums.Occupation
+{
+    public enum Occupation
+    {
+        Actor,
+        Editor,
+        Cinematographer,
+        FilmDirector,
+        FilmProducer,
+        FilmScoreComposer,
+        Screenwriter,
+        Publisher,
+        TelevisionDirector,
+        TelevisionProducer,
+        Writer,
+        VoiceActor
+    }
+
+    public static class OccupationEnumExtensions
+    {
+        public static Occupation? GetOccupation(string actorEnum)
+        {
+            return actorEnum switch
+            {
+                "actor" => Occupation.Actor,
+                "cinematographer" => Occupation.Cinematographer,
+                "editor" => Occupation.Editor,
+                "film director" => Occupation.FilmDirector,
+                "film producer" => Occupation.FilmProducer,
+                "film score composer" => Occupation.FilmScoreComposer,
+                "publisher" => Occupation.Publisher,
+                "screenwriter" => Occupation.Screenwriter,
+                "television director" => Occupation.TelevisionDirector,
+                "television producer" => Occupation.TelevisionProducer,
+                "writer" => Occupation.Writer,
+                "voice actor" => Occupation.VoiceActor,
+                _ => null
+            };
+        }
+    }
+}
