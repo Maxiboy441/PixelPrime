2fed17b6026e7e5f71f63099f2877c3d49788b7d
Felipe Pedroso Ramos <felipe.pedroso-ramos@odt.net>
2024-09-15 15:45:07 +0200

    feat(79102517): add dropdown menu feature

--- Project/Project/wwwroot/js/site.js
+++ Project/Project/wwwroot/js/site.js
@@ -150,6 +150,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
             flashContainer.classList.add('fade');
         }, 3500);
     }
+
+
+    const dropdownToggle = document.getElementById('navbarDropMenu');
+    const dropdownMenu = dropdownToggle.nextElementSibling;
+
+    dropdownToggle.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownMenu.classList.toggle('show');
+    });
+
+    document.addEventListener('click', function(event) {
+        const isClickInside = dropdownToggle.contains(event.target) || dropdownMenu.contains(event.target);
+
+        if (!isClickInside) {
+            dropdownMenu.classList.remove('show');
+        }
+    });
 });
 
 // Movies Slider
